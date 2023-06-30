7cec3facec195b258d9a4fbb59c7b8d21420eac6
sami <s.a.sami359359@gmail.com>
2023-06-30 11:24:30 +0530

    modified firestore

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 import {getStorage} from "firebase/storage"
 import { GoogleAuthProvider, signInWithPopup, signInWithRedirect, signOut, onAuthStateChanged } from "firebase/auth";
 const firebaseConfig = {
-    apiKey: "AIzaSyCJG_PhABu_NYfwMEm-f2NCysrNzCiKOQU",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: "chaikathela7.firebaseapp.com",
     projectId: "chaikathela7",
     storageBucket: "chaikathela7.appspot.com",
     messagingSenderId: "165004505667",
-    appId: "1:165004505667:web:a8a76ae36e945169b29af6"
+    appId: process.env.REACT_APP_APP_ID
 };
 
 //init
